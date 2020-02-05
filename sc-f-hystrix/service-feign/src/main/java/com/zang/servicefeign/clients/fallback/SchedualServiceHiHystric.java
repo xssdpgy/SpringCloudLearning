a8fc3cb97866280c197fd36962be6d36243b28bc
xssdpgy <2234501359@qq.com>
2020-02-05 09:17:20 +0800

    （三）Spring Cloud教程——Hystrix（F版本） Codes

---
+++ sc-f-hystrix/service-feign/src/main/java/com/zang/servicefeign/clients/fallback/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.zang.servicefeign.clients.fallback;
+
+import com.zang.servicefeign.clients.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry, you are fail,"+name;
+    }
+}
