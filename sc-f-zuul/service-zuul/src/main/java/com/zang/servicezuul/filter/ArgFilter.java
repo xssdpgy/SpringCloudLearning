4c66d8d1884b1b48729137151c3226ce6146db35
xssdpgy <2234501359@qq.com>
2020-02-06 20:48:33 +0800

    （四）Spring Cloud教程——Zuul（F版本） Codes

---
+++ sc-f-zuul/service-zuul/src/main/java/com/zang/servicezuul/filter/ArgFilter.java
@@ -0,0 +1,54 @@
+package com.zang.servicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ArgFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        // 前置过滤器
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        // 优先级为0，数字越大，优先级越低
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        // 是否执行该过滤器，此处为true，说明需要过滤
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
