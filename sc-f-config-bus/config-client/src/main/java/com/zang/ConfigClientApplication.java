7be7a64550c0b66611a0ae13a3071342c400eade
xssdpgy <2234501359@qq.com>
2020-02-12 23:38:38 +0800

    （五）Spring Cloud教程——Config（F版本）—— 4. Spring Cloud Bus  Codes

---
+++ sc-f-config-bus/config-client/src/main/java/com/zang/ConfigClientApplication.java
@@ -0,0 +1,32 @@
+package com.zang;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+	/**
+	 * http://localhost:8881/actuator/bus-refresh
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
